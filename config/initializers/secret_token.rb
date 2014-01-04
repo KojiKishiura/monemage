78edc37df6c381101990c26e78ce0da77a87ccdf
KojiKishiura <kk00305@gmail.com>
2014-01-04 11:27:02 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monemage::Application.config.secret_key_base = '58a2b287560cfacb0656d3c532299d3a3448ba3aea407d1ce0d7b978f150e4af7690a9e675b7b21e478bce6f253ebe58c16e14e5ccbc2fda948ef78264b98b77'
